7b12ddfdf68a63941a7a84545c59f353f9ba3a44
cakirburak <cakirburak43@hotmail.com>
2024-02-26 23:58:24 +0300

    project folder restructure for maven build

--- inventory-service/src/main/java/com/jrp/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/jrp/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,36 @@
 package com.jrp.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.jrp.inventoryservice.model.Inventory;
+import com.jrp.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadInventoryData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			// sku code for iphone 13
+			inventory.setSkuCode("123456");
+			inventory.setQuantity(100);
+
+			// sku code for iphone 14
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("654321");
+			inventory2.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory2);
+		};
 	}
 
 }
