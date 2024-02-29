a22093fc83bd72b092c0a9d8fc5ce6cd32adef9d
cakirburak <cakirburak43@hotmail.com>
2024-02-29 15:52:21 +0300

    Eureka discovery server implemented

--- inventory-service/src/main/java/com/jrp/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/jrp/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.jrp.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.jrp.inventoryservice.model.Inventory;
 import com.jrp.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
